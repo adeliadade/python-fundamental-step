c607099332219443d8cb9aa8d1d10fb561ba7f67
Administrator <zahraadelia1198@gmail.com>
2022-12-09 15:47:42 +0700

    initial commit

---
+++ praktek bentuk perulangan dengan for.py
@@ -0,0 +1,11 @@
+"""
+program perulangan penjualan gelang
+"""
+
+jumlah_stok_gelang = 40
+print('owner berkata "jual semua stok gelang"')
+
+jumlah_stok_gelang_yang_sudah_terjual = 0
+
+for jumlah_stok_gelang_yang_sudah_terjual in range (1, jumlah_stok_gelang+1):
+    print(f'stok gelang ke {jumlah_stok_gelang_yang_sudah_terjual} sudah terjual')
